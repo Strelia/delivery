dffdef219e145762347f73010a204429c30ad4a9
gerbi <strelchenko.vitaliy.95@gmail.com>
2021-02-03 21:32:54 +0200

    Added config for dashboard, new menu item and translation menu

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Admin;
+use App\Entity\Business;
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
@@ -22,14 +24,42 @@ class DashboardController extends AbstractDashboardController
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('App');
+            ->setTitle('Delivery')
+            // the name visible to end users
+            // you can include HTML contents too (e.g. to link to an image)
+//            ->setTitle('<img src="..."> ACME <span class="text-small">Corp.</span>')
+
+            // the path defined in this method is passed to the Twig asset() function
+//            ->setFaviconPath('favicon.svg')
+
+            // the domain used by default is 'messages'
+            ->setTranslationDomain('messages')
+
+            // there's no need to define the "text direction" explicitly because
+            // its default value is inferred dynamically from the user locale
+            ->setTextDirection('ltr')
+
+            // set this option if you prefer the page content to span the entire
+            // browser width, instead of the default design which sets a max width
+            ->renderContentMaximized()
+
+            // set this option if you prefer the sidebar (which contains the main menu)
+            // to be displayed as a narrow column instead of the default expanded design
+            ->renderSidebarMinimized()
+
+            // by default, all backend URLs include a signature hash. If a user changes any
+            // query parameter (to "hack" the backend) the signature won't match and EasyAdmin
+            // triggers an error. If this causes any issue in your backend, call this method
+            // to disable this feature and remove all URL signature checks
+            ->disableUrlSignatures();
     }
 
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::linkToCrud('Admins', 'fa fa-user-shield', User::class);
-        yield MenuItem::linkToCrud('User', 'fa fa-user', User::class);
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linkToCrud('menu.admin.list', 'fa fa-user-shield', User::class)
+            ->setController(AdminCrudController::class);
+        yield MenuItem::linkToCrud('menu.user', 'fas fa-user', User::class);
+        yield MenuItem::linkToCrud('menu.business', 'fas fa-business-time', Business::class);
     }
 }
