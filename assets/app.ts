abfd1fe7deada69c38f88fca494246292aa01a01
gerbi <strelchenko.vitaliy.95@gmail.com>
2021-02-15 17:52:21 +0200

    added assets, encore

---
+++ assets/app.ts
@@ -0,0 +1,13 @@
+// JS
+import $ from 'jquery';
+import 'bootstrap';
+
+// CSS
+import 'bootstrap/scss/bootstrap.scss';
+import '@fortawesome/fontawesome-free/scss/solid.scss';
+import '@fortawesome/fontawesome-free/scss/regular.scss';
+import '@fortawesome/fontawesome-free/scss/fontawesome.scss';
+import './styles/app.scss';
+
+// start the Stimulus application
+import './bootstrap';
